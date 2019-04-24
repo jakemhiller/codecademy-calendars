ebb9bef9e9b8be6311e8622dea0101178d17b7c4
joshuakgoldberg <joshuakgoldberg@outlook.com>
2019-04-24 10:55:27 -0400

    Added redirects for pages

---
+++ redirect.js
@@ -0,0 +1,8 @@
+(function () {
+    var oldUrlBase = '//ryzacinc.github.io'
+    var newUrlBase = '//ryzacinc.github.io';
+    var newUrl = window.location.toString().replace(oldUrlBase, newUrlBase);
+
+    console.log("Redirecting to: " + newUrl);
+    window.location = newUrl;
+})();
